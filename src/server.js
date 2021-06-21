f565c335dc7f6d328e0b45e9b89c422d9ed9807e
syk050 <syk050@naver.com>
2021-06-21 17:26:49 +0900

    child_process add

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const app = require('./app');
 const req = require('./util/request-bitstamp');
+const {execute} = require('./util/modelExec');
+
 
 app.listen(app.get('port'));
 console.log('Server is in port', app.get('port'));
 
 // req.selectLastOne();
+execute();
---
+++ src/util/modelExec.js
@@ -0,0 +1,31 @@
+const modelExec = {}
+
+const { spawn } = require('child_process');
+const path = require('path');
+
+
+modelExec.execute = () => {
+    filePath = path.join(__dirname, '/../py/code/test_prototype.py');
+    // filePath = path.join(__dirname, '/../py/spawnTest.py');
+    // console.log(filePath);
+
+    const model = spawn('python', [filePath, '1m']);
+    model.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    model.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    model.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+
+    model.on('error', (error => {
+        console.log(`error: ${error.message}`);
+    }))
+}
+
+
+module.exports = modelExec;
