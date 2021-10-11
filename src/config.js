311732e56e2527d65511b4c34fdf6541930f9d54
root <root@ip-172-31-5-133.ap-northeast-2.compute.internal>
2021-10-11 08:19:34 +0000

    commit

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
     database: {
         connectionLimit: 10,
         host: 'localhost',
-        user: 'root',
+        user: 'system',
         password: '1234',
         database: 'bitcoin',
         multipleStatements: true
--- src/trade/timecheck.js
+++ src/trade/timecheck.js
@@ -4,7 +4,6 @@ const { parentPort } = require('worker_threads');
 setInterval(function() {
     var date = new Date();
     if ( date.getSeconds() === 0 ) {
-        date.setMinutes(date.getMinutes() - 1);
         parentPort.postMessage(date);
     }
   }, 1000);
