a4ff137bcd8779ff759eb370a5d638fa62cc476f
syk050 <syk050@naver.com>
2021-11-08 14:31:31 +0900

    commit

--- controller/trade.controller.js
+++ controller/trade.controller.js
@@ -2,25 +2,25 @@ const tradeCtrl = {};
 
 const tradeLog = require('../models/trade-log');
 
-tradeCtrl.getTrade = async function(req, res){
-    tradeLog.find({})
-      .sort('{timestamp: -1}')
-      .exec((err, logs) => {
-        if (err) return res.json(err);
-  
-        res.send(logs);
-    });
-};
-
 // tradeCtrl.getTrade = async function(req, res){
 //     tradeLog.find({})
 //       .sort('{timestamp: -1}')
 //       .exec((err, logs) => {
 //         if (err) return res.json(err);
   
-//         res.render('trade-log', {logs: logs});
+//         res.send(logs);
 //     });
 // };
+
+tradeCtrl.getTrade = async function(req, res){
+    tradeLog.find({})
+      .sort('{timestamp: -1}')
+      .exec((err, logs) => {
+        if (err) return res.json(err);
+  
+        res.render('trade-log', {logs: logs});
+    });
+};
 
 tradeCtrl.postTrade = async function(req, res){
     // tradeLog.create(req.body, (err, contact) => {
